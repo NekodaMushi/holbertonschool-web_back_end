2ba1ed6f6befdf9fd344fa74068eed8eb656c3e8
NekodaMushi <study.fabien@gmail.com>
2023-09-19 10:09:01 +0200

    doc length restriction

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -8,10 +8,12 @@ from typing import List, Union
 
 def sum_mixed_list(nxd_lst: List[Union[int, float]]) -> float:
     """
-    Calculates the sum of a list of numbers (integers and/or floats) and returns it as a float.
+    Calculates the sum of a list of numbers
+      (integers and/or floats) and returns it as a float.
 
     Args:
-        nxd_lst (List[Union[int, float]]): The list of numbers (integers and/or floats) to be summed.
+        nxd_lst (List[Union[int, float]]):
+          The list of numbers (integers and/or floats) to be summed.
 
     Returns:
         float: The sum of the numbers in 'nxd_lst'.
