14178c52f61bf855c8c8d5131e8c5b37f08d6828
NekodaMushi <study.fabien@gmail.com>
2023-09-19 11:43:56 +0200

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
+"""
+    Async annotations - wait_n
+"""
 
 from typing import List
-import asyncio
+
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously waits for 'n' random delays
+      and returns a sorted list of delay times.
+
+    Args:
+        n (int): The number of delays to wait for.
+        max_delay (int): The maximum delay time in seconds.
+
+    Returns:
+        List[float]: A sorted list of 'n' random delay times in seconds.
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
 
