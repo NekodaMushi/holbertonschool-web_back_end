bdcad19650cf951d9a9f9642f2e18aef12570151
NekodaMushi <study.fabien@gmail.com>
2023-09-19 14:48:55 +0200

    Good to be back on Python

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Async annotations - async_generator
+"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    Asynchronous generator that yields random floats between 0 and 10.
+
+    Yields:
+        float: A random float between 0 and 10.
+    """
+    for x in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
