3e995b568dbce50a8f567b9be0df17941c152da0
NekodaMushi <study.fabien@gmail.com>
2023-09-20 09:34:55 +0200

    Interesting way to loop

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+"""
+    Async comprehension - async_comprehension
+"""
 
+from typing import List
+
+# Import the async_generator function
 async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Asynchronously generates and returns a list of floats using async comprehension.
+
+    Returns:
+        List[float]: A list of floats generated asynchronously.
+    """
+    return [number async for number in async_generator()]
