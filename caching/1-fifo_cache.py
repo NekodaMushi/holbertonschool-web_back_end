52ff23e0185bfd9071b3bdb0acb8fec27f1e6d6e
NekodaMushi <study.fabien@gmail.com>
2023-09-21 10:20:08 +0200

    doc

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
+"""
+FIFO caching - FIFOCache class
+"""
 
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """
+    A caching class that implements a First-In-First-Out
+      (FIFO) eviction policy.
+
+    Methods:
+        __init__(self): Initialize the FIFOCache.
+        put(self, key, item): Store an item in the cache.
+        get(self, key): Retrieve an item from the cache.
+
+    Attributes:
+        cache_data (dict): A dictionary to store cached data.
+        keys (list): A list to maintain the order
+          of keys based on the FIFO policy.
+    """
+
     def __init__(self):
+        """
+        Initialize the FIFOCache.
+        """
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
+        """
+        Store an item in the cache.
+
+        Args:
+            key: The key for the item.
+            item: The item to be stored.
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
         if key not in self.keys:
@@ -19,6 +47,15 @@ class FIFOCache(BaseCaching):
             print(f"DISCARD: {discarded_key}")
 
     def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Args:
+            key: The key for the item to retrieve.
+
+        Returns:
+            The cached item if found, otherwise None.
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
