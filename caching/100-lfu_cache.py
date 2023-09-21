8c8aea4086504ff7586a984bc42ece2cf3630ea7
NekodaMushi <study.fabien@gmail.com>
2023-09-21 13:21:13 +0200

    PEP8

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
+"""
+LFU caching - LFUCache class
+"""
 
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
+    """
+    A caching class that implements a Least Frequently Used (LFU)
+    eviction policy.
+
+    Methods:
+        __init__(self): Initialize the LFUCache.
+        put(self, key, item): Store an item in the cache.
+        get(self, key): Retrieve an item from the cache.
+
+    Attributes:
+        cache_data (dict): A dictionary to store cached data.
+        frequency_keys (dict): A dictionary to track the frequency of
+        keys.
+    """
+
     def __init__(self):
+        """
+        Initialize the LFUCache.
+        """
         super().__init__()
         self.frequency_keys = {}
 
     def put(self, key, item):
+        """
+        Store an item in the cache.
+
+        Args:
+            key: The key for the item.
+            item: The item to be stored.
+        """
         if key is None or item is None:
             return
 
@@ -25,10 +53,6 @@ class LFUCache(BaseCaching):
                 self.frequency_keys, key=lambda x: self.frequency_keys[x]
             )
 
-            print("-----")
-            for x in self.frequency_keys:
-                print(f"{x} -> {self.frequency_keys[x]}")
-            print("-----")
             del self.cache_data[discarded_key]
             del self.frequency_keys[discarded_key]
             print(f"DISCARD: {discarded_key}")
@@ -37,6 +61,15 @@ class LFUCache(BaseCaching):
                 self.frequency_keys[immune_new_key] = temp_safe_for_key
 
     def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Args:
+            key: The key for the item to retrieve.
+
+        Returns:
+            The cached item if found, otherwise None.
+        """
         if key is None or key not in self.cache_data:
             return None
         self.frequency_keys[key] += 1
