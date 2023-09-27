e7e521b07b72806d81242f09930368cf904ac895
NekodaMushi <study.fabien@gmail.com>
2023-09-27 16:03:18 +0200

    doc

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -21,6 +21,6 @@ def index_range(page, page_size):
         return None
 
     index_start = (page - 1) * page_size
-    index_end = index_start + page_size - 1
+    index_end = index_start + page_size
 
     return index_start, index_end
