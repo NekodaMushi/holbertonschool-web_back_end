e641aace15814f473c2c32395f244d43600ed9d9
NekodaMushi <study.fabien@gmail.com>
2023-10-01 10:58:27 +0200

    last dict

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """
+    Server class for deletion-resilient hypermedia pagination.
+    """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset.
+
+        Returns:
+            List[List]: The dataset containing rows of data.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """
+        Create an indexed dataset for efficient retrieval.
+
+        Returns:
+            Dict[int, List]: A dictionary where keys are indices and values
+            are corresponding rows of data.
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        assert index is None or (
+            isinstance(index, int) and index >= 0
+        ), "Index should be a non-negative integer"
+        assert (
+            isinstance(page_size, int) and page_size > 0
+        ), "Page size should be a positive integer"
+
+        total_rows = len(self.dataset())
+
+        next_index = index + page_size
+
+        # Making sure start index is within a valid range
+        if index is None:
+            start_index = 0
+        else:
+            start_index = max(0, min(index, total_rows - 1))
+
+        # Making sure next index is within a valid range
+        next_index = max(0, min(next_index, total_rows))
+
+        # Retrieve data for the current page, checking if the index exists
+        data = [
+            self.indexed_dataset().get(i)  # Use .get() to avoid KeyError
+            for i in range(start_index, min(start_index + page_size, total_rows))
+        ]
+
+        hyper_dict = {
+            "index": start_index,
+            "data": data,
+            "next_index": next_index,
+            "page_size": page_size,
+        }
+
+        return hyper_dict
