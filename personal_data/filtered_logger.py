3ee41303f2cc40a7a0130fa3a87948bfa064594b
NekodaMushi <study.fabien@gmail.com>
2023-10-04 12:09:34 +0200

    getting harder

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,26 @@
 
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
+
+        return super().format(record)
 
 
 def filter_datum(
@@ -29,7 +49,6 @@ def filter_datum(
     """
     for field in fields:
         message = re.sub(
-            field + "=.+?" + separator,
-            field + "=" + redaction + separator, message
+            field + "=.+?" + separator, field + "=" + redaction + separator, message
         )
     return message
