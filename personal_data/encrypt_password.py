c67d424594bce86a40a533af17e653c52e67bea7
NekodaMushi <study.fabien@gmail.com>
2023-10-06 09:35:49 +0200

    doc

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
 """
-  Encrypting passwords
+Encrypting passwords
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password as bytes.
+    """
     salt = bcrypt.gensalt(rounds=14)
-
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-
     return hashed_password
