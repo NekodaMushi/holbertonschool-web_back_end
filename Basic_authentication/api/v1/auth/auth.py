9f48e1f2f1569ea40f5bfcc3a75df096a580b3a7
NekodaMushi <study.fabien@gmail.com>
2023-10-07 15:20:02 +0200

    class Auth

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Init auth requirement"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Init authorization requirement"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Init user requirement"""
+        return None
