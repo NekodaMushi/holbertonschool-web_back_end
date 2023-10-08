f4fa8c748744e425922f1ae1318d8ad3ae5a7c83
NekodaMushi <study.fabien@gmail.com>
2023-10-08 09:58:46 +0200

    slash tolerant

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,20 @@ from typing import List, TypeVar
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Init auth requirement"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == "":
+            return True
+
+        slash_tolerant_path = path if path.endswith("/") else path + "/"
+
+        slash_tolerant_excluded_path = [
+            path if path.endswith("/") else path + "/" for path in excluded_paths
+        ]
+
+        if slash_tolerant_path in slash_tolerant_excluded_path:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Init authorization requirement"""
