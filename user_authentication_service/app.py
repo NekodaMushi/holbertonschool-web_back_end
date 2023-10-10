06c6848ef748504693b005abe7eddd7961ba2954
NekodaMushi <study.fabien@gmail.com>
2023-10-10 13:54:40 +0200

    Update app.py

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 
 """Basic flask app"""
 
-from flask import Flask, jsonify, request, make_response, abort
+from flask import Flask, jsonify, request, make_response, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -42,6 +42,20 @@ def login():
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            response = redirect("/")
+            response.delete_cookie("session_id")
+            return response
+        else:
+            abort(403)
 
 
 if __name__ == "__main__":
