573f3624162ff856dc28eaf187da1d17a47051fa
NekodaMushi <study.fabien@gmail.com>
2023-10-10 12:26:56 +0200

    come on checker

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -73,7 +73,8 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         try:
-            self._db.find_user_by(user_id=user_id)
-            self._db.update_user(user_id, session_id=None)
+            user = self._db.find_user_by(id=user_id)
         except NoResultFound:
             return None
+        else:
+            self._db.update_user(user.id, session_id=None)
