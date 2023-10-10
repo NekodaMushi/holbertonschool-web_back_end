a0c93a5f470ceeca9e0a27bde628cddc8ef2d224
NekodaMushi <study.fabien@gmail.com>
2023-10-10 10:05:59 +0200

    sessions route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 
 """Basic flask app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -26,6 +26,27 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password) == True:
+        session_id = AUTH.create_session(email)
+        response = make_response(
+            jsonify({"email": "<user email>", "message": "logged in"})
+        )
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+    # try:
+    #     user = AUTH.valid_login(email, password)
+    #     return jsonify({"email": email, "password": password, "message": "are correct"})
+    # except
 
 
 if __name__ == "__main__":
