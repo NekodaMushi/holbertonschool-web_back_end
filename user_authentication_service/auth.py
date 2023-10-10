5db1b0964b0831010178ef1ffdc8ad20d9353031
NekodaMushi <study.fabien@gmail.com>
2023-10-10 12:22:27 +0200

    Update auth.py

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,5 +76,5 @@ class Auth:
             self._db.find_user_by(user_id=user_id)
         except NoResultFound:
             return None
-        else:
-            self._db.update_user(user_id, session_id=None)
+
+        self._db.update_user(user_id, session_id=None)
