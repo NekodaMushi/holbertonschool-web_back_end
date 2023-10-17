b76470a468a99f7f547584f6d0c5c4340476ea8d
NekodaMushi <study.fabien@gmail.com>
2023-10-17 10:39:14 +0200

    Update test_utils.py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -49,6 +49,7 @@ class TestGetJson(unittest.TestCase):
         ]
     )
     def test_get_json(self, test_url, test_payload):
+        """Fn test"""
         with patch("requests.get") as mock_method:
             mock_method.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
