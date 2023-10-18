e4509f5e2959ae6d39beb0fd161b5fb553422ecf
NekodaMushi <study.fabien@gmail.com>
2023-10-18 12:32:39 +0200

    Update 5-app.py

---
+++ i18n/5-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""Basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Config class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Render html template"""
+    return render_template("4-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determine best match with supported language"""
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user():
+    """
+    get_user function
+    """
+    user_id = request.args.get("login_as")
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
