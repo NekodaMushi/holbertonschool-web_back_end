76267c0825b010d3f1ce74247344d9033fcceb11
NekodaMushi <study.fabien@gmail.com>
2023-10-18 12:10:32 +0200

    test

---
+++ i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""Basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Render html template"""
+    return render_template("2-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determine best match with supported language"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
