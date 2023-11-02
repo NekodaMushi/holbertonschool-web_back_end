72b62615b0fe59574543971666580037a49ad144
NekodaMushi <study.fabien@gmail.com>
2023-11-02 09:49:50 +0100

    Create 1-country_users.sql

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- create users table
+-- attributes are id email & name
+CREATE TABLE IF NOT EXISTS users
+(
+  id serial NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email character varying(255) NOT NULL UNIQUE,
+  name character varying(255),
+  country  ENUM('US','CO','TN') NOT NULL,
+)
