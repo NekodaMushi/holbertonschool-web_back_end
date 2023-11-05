1d8ef33fb97d1809f65eba556ffd19afcc22f9dd
NekodaMushi <study.fabien@gmail.com>
2023-11-05 09:28:16 +0100

    Update 1-country_users.sql

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users
 (
   id serial NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  email character varying(255) NOT NULL UNIQUE,
+  email character varying(255) NOT NULL,
   name character varying(255),
   country ENUM('US','CO','TN') NOT NULL,
+  UNIQUE (email)
 )
