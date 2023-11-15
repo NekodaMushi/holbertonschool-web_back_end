06c14f46809fe879441444d5fcab950e105b44d0
NekodaMushi <study.fabien@gmail.com>
2023-11-15 09:59:49 +0100

    easy js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default /*async */ function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const user = signUpUser();
+  const photo = uploadPhoto();
+
+  return Promise.allSettled([user, photo]).then((values) => {
+    values.map((v) => ({
+      status: v.status,
+      value:
+        v.status === "fulfilled"
+          ? v.value
+          : `${v.reason.name}: ${v.reason.message}`,
+    }));
+  });
+}
