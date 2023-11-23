1df72f37cb8aff2f52b7b1c698a02248f052038e
NekodaMushi <study.fabien@gmail.com>
2023-11-23 14:25:35 +0100

    task 2

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
-export default function get_list_students() {
+export default function getListStudents() {
   const array = [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
     { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
+  return array;
 }
