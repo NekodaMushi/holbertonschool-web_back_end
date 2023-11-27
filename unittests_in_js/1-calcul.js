fb79cc59c345d51992fa29aac328afe84fcfc0ba
NekodaMushi <study.fabien@gmail.com>
2023-11-27 13:11:12 +0100

    typo corrected

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,7 +1,7 @@
 function calculateNumber(type, x, y) {
   const operation = {
     SUM: Math.round(x) + Math.round(y),
-    SUBSTRACT: Math.round(x) - Math.round(y),
+    SUBTRACT: Math.round(x) - Math.round(y),
     DIVIDE: Math.round(y) === 0 ? "Error" : Math.round(x) / Math.round(y),
   };
   return operation[type] || "Wrong operator chosen";
