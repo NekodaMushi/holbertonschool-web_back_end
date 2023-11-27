0c8ae1b215f9f2c0e75d84656539b9ca12841e9d
NekodaMushi <study.fabien@gmail.com>
2023-11-27 09:31:09 +0100

    filter and map

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students;
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradEl = newGrades.find((el) => el.studentId === student.id);
+
+      if (gradEl) {
+        student.grade = gradEl.grade;
+      } else {
+        student.grade = "N/A";
+      }
+      return student;
+    });
 }
