af714d1e7ea2875332bbc12b5d5ac60f3d3af33a
NekodaMushi <study.fabien@gmail.com>
2023-11-27 13:10:38 +0100

    more test

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,10 @@
+function calculateNumber(type, x, y) {
+  const operation = {
+    SUM: Math.round(x) + Math.round(y),
+    SUBSTRACT: Math.round(x) - Math.round(y),
+    DIVIDE: Math.round(y) === 0 ? "Error" : Math.round(x) / Math.round(y),
+  };
+  return operation[type] || "Wrong operator chosen";
+}
+
+module.exports = calculateNumber;
