763df0ebf3adb80a6089eb3cec11d8498dec0965
NekodaMushi <study.fabien@gmail.com>
2023-12-02 16:40:58 +0100

    Update 2-calcul_chai.test.js

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,43 +1,82 @@
-const expect = require("chai").expect;
-const calculateNumber = require("./1-calcul");
+var chai = require("chai");
+const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber", function () {
-  describe("SUM", function () {
-    it("should return the sum of two rounded numbers", function () {
-      expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
+  describe("SUM no Round", function () {
+    it("should return 5", function () {
+      chai.expect(calculateNumber("SUM", 1, 4)).to.equal(5);
     });
   });
 
-  describe("SUBTRACT", function () {
-    it("should round the first argument", function () {
-      expect(calculateNumber("SUBTRACT", 1.0, 0)).to.equal(1);
-      expect(calculateNumber("SUBTRACT", 1.3, 0)).to.equal(1);
-      expect(calculateNumber("SUBTRACT", 1.7, 0)).to.equal(2);
-    });
-
-    it("should round the second argument", function () {
-      expect(calculateNumber("SUBTRACT", 0, 1.0)).to.equal(-1);
-      expect(calculateNumber("SUBTRACT", 0, 1.3)).to.equal(-1);
-      expect(calculateNumber("SUBTRACT", 0, 1.7)).to.equal(-2);
-    });
-
-    it("should return the right number", function () {
-      expect(calculateNumber("SUBTRACT", 1.3, 0)).to.equal(1);
-      expect(calculateNumber("SUBTRACT", 0, 1.2)).to.equal(-1);
-      expect(calculateNumber("SUBTRACT", 1.3, 1.3)).to.equal(0);
-      expect(calculateNumber("SUBTRACT", 1.7, 1.2)).to.equal(1);
-      expect(calculateNumber("SUBTRACT", 1.3, 1.8)).to.equal(-1);
-      expect(calculateNumber("SUBTRACT", 1.6, 1.8)).to.equal(0);
+  describe("SUM first round", function () {
+    it("should return 6", function () {
+      chai.expect(calculateNumber("SUM", 2.4, 4)).to.equal(6);
     });
   });
 
-  describe("DIVIDE", function () {
-    it("should return the division of two rounded numbers", function () {
-      expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+  describe("SUM second round ", function () {
+    it("should return 6", function () {
+      chai.expect(calculateNumber("SUM", 4, 2.4)).to.equal(6);
     });
+  });
 
-    it("should return 'Error' when dividing by 0", function () {
-      expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
+  describe("SUM both round", function () {
+    it("should return 6", function () {
+      chai.expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe("SUBTRACT no round", function () {
+    it("should return 2", function () {
+      chai.expect(calculateNumber("SUBTRACT", 5, 3)).to.equal(2);
+    });
+  });
+
+  describe("SUBTRACT first round", function () {
+    it("should return -3", function () {
+      chai.expect(calculateNumber("SUBTRACT", 2, 4.5)).to.equal(-3);
+    });
+  });
+
+  describe("SUBTRACT second round", function () {
+    it("should return 3", function () {
+      chai.expect(calculateNumber("SUBTRACT", 4.5, 2)).to.equal(3);
+    });
+  });
+
+  describe("SUBTRACT both round", function () {
+    it("should return -4", function () {
+      chai.expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe("DIVIDE no round", function () {
+    it("should return 2", function () {
+      chai.expect(calculateNumber("DIVIDE", 8, 4)).to.equal(2);
+    });
+  });
+
+  describe("DIVIDE first round", function () {
+    it("should return 5", function () {
+      chai.expect(calculateNumber("DIVIDE", 9.5, 2)).to.equal(5);
+    });
+  });
+
+  describe("DIVIDE second round", function () {
+    it("should return 0.2", function () {
+      chai.expect(calculateNumber("DIVIDE", 2, 9.5)).to.equal(0.2);
+    });
+  });
+
+  describe("DIVIDE both round", function () {
+    it("should return 0.2", function () {
+      chai.expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+    });
+  });
+
+  describe("DIVIDE Error", function () {
+    it("should return Error", function () {
+      chai.expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
     });
   });
 });
