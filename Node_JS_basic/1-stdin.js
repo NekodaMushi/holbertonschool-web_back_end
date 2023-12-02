3705f509146039dcf27af9745ff3f9dedff6f427
Fabien Pineau <98282927+NekodaMushi@users.noreply.github.com>
2023-12-02 16:22:19 +0100

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) process.stdout.write(`Your name is: ${name}`);
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
