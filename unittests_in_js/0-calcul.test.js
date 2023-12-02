ff7f04f537a660245d30148ce8f6adac3daf4cb9
NekodaMushi <study.fabien@gmail.com>
2023-12-02 16:40:08 +0100

    Update 0-calcul.test.js

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -8,4 +8,13 @@ describe("calculateNumber", function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it("should return 4 when passed 1 and 3.4", function () {
+    assert.strictEqual(calculateNumber(1, 3.4), 4);
+  });
+  it("should return 2 when passed -1.4 and 3", function () {
+    assert.strictEqual(calculateNumber(-1.4, 3), 2);
+  });
+  it("should return -2 when passed 1 and -3.4", function () {
+    assert.strictEqual(calculateNumber(1, -3.4), -2);
+  });
 });
